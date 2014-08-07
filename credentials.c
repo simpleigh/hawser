e1db379ad805722662fbadfdd78b2d4292ea42a9
Leigh Simpson <code@simpleigh.com>
2014-08-07 20:14:45 +0100

    Added code to read credentials from environment variables

--- credentials.c
+++ credentials.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct Credentials {
 	char access_key_id[21];
@@ -19,8 +21,36 @@ credentials_get_secret_access_key()
 	return credentials.secret_access_key;
 }
 
+typedef struct Env {
+	const char *name;
+	char *destination;
+	size_t length;
+} Env;
+
+const Env ENV[] = {
+	{ "AWS_ACCESS_KEY_ID",     credentials.access_key_id,     20 },
+	{ "AWS_SECRET_ACCESS_KEY", credentials.secret_access_key, 40 }
+};
+
+void
+credentials_load_environment()
+{
+	size_t cFields = sizeof(ENV) / sizeof(Env);
+	size_t i;
+	char *source;
+
+	for (i = 0; i < cFields; i++) {
+		source = getenv(ENV[i].name);
+		if (source != NULL) {
+			memcpy(ENV[i].destination, source, ENV[i].length);
+			ENV[i].destination[ENV[i].length] = '\0';
+		}
+	}
+}
+
 int main(void)
 {
+	credentials_load_environment();
 	printf("Access Key ID: %s\n",     credentials_get_access_key_id());
 	printf("Secret access key: %s\n", credentials_get_secret_access_key());
 	return 0;
