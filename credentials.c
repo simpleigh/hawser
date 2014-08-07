311f753d18c7c83dd462777e2d0fed61b826528e
Leigh Simpson <code@simpleigh.com>
2014-08-07 19:58:20 +0100

    Created struct to hold credentials

--- credentials.c
+++ credentials.c
@@ -1,4 +1,15 @@
+#include <stdio.h>
+
+typedef struct Credentials {
+	char access_key_id[21];
+	char secret_access_key[41];
+} Credentials;
+
+static Credentials credentials = { "", "" };
+
 int main(void)
 {
+	printf("Access Key ID: %s\n",     credentials.access_key_id);
+	printf("Secret access key: %s\n", credentials.secret_access_key);
 	return 0;
 }
