2db5da8f3833612838668e47c58a73d59b37dc55
Leigh Simpson <code@simpleigh.com>
2014-08-10 20:37:35 +0100

    Modified file code to assemble regexes in standard way

--- lib/credentials/file.c
+++ lib/credentials/file.c
@@ -5,30 +5,95 @@
 #include <stdlib.h>
 
 typedef struct FileEntry {
-	const char * const regex;
+	const char * const name;
+	const char * const pattern;
 	regex_t *compiled;
 	void (* const store)(const char *);
 } FileEntry;
 
 static FileEntry FILE_ENTRIES[] = {
 	{
-		"^[ \t]*access_key_id[ \t]*=[ \t]*([^ \t]{20})[ \t]*",
+		"access_key_id",
+		"[^ \t]{20}",
 		NULL,
 		credentials_set_access_key_id
 	},
 	{
-		"^[ \t]*secret_access_key[ \t]*=[ \t]*([^ \t]{40})[ \t]*",
+		"secret_access_key",
+		"[^ \t]{40}",
 		NULL,
 		credentials_set_secret_access_key
 	},
 	{
 		NULL,
 		NULL,
+		NULL,
 		NULL
 	}
 };
 
-#define LINE_BUFFER 80
+static void
+strappend(char **pszDestination, const char *szSource)
+{
+	while (*szSource) {
+		**pszDestination = *szSource;
+		(*pszDestination)++;
+		szSource++;
+	}
+}
+
+static void
+free_regex(FileEntry *fileEntry)
+{
+	regfree(fileEntry->compiled);
+	free(fileEntry->compiled);
+	fileEntry->compiled = NULL;
+}
+
+#define REGEX_BUFFER 80
+
+static void
+compile_regexes(FileEntry *fileEntry)
+{
+	char szRegex[REGEX_BUFFER];
+	char *szPointer;
+	int result;
+
+	while (fileEntry->name) {
+		szPointer = szRegex;
+		strappend(&szPointer, "^[ \t]*");
+		strappend(&szPointer, fileEntry->name);
+		strappend(&szPointer, "[ \t]*=[ \t]*(");
+		strappend(&szPointer, fileEntry->pattern);
+		strappend(&szPointer, ")[ \t]*");
+		*szPointer = '\0';
+
+		fileEntry->compiled = malloc(sizeof(regex_t));
+		if (fileEntry->compiled) {
+			result = regcomp(fileEntry->compiled, szRegex, REG_EXTENDED);
+			if (result) {
+				free_regex(fileEntry);
+			}
+		}
+
+		fileEntry++;
+	}
+}
+
+#undef REGEX_BUFFER
+
+static void
+free_regexes(FileEntry *fileEntry)
+{
+	while (fileEntry->name) {
+		if (fileEntry->compiled) {
+			free_regex(fileEntry);
+		}
+		fileEntry++;
+	}
+}
+
+#define LINE_BUFFER 200
 
 void
 credentials_load_file(const char *filename)
@@ -43,33 +108,25 @@ credentials_load_file(const char *filename)
 		return;
 	}
 
-	fileEntry = FILE_ENTRIES;
-	while (fileEntry->regex) {
-		fileEntry->compiled = malloc(sizeof(regex_t));
-		if (fileEntry->compiled) {
-			regcomp(fileEntry->compiled, fileEntry->regex, REG_EXTENDED);
-		}
-		fileEntry++;
-	}
+	compile_regexes(FILE_ENTRIES);
 
 	while (fgets(szLine, LINE_BUFFER, stream)) {
 		fileEntry = FILE_ENTRIES;
-		while (fileEntry->regex) {
+		while (fileEntry->name) {
 			regmatch_t matchptr[2];
-			int result = regexec(fileEntry->compiled, szLine, 2, matchptr, 0);
-			if (result == 0) {
-				szLine[matchptr[1].rm_eo] = '\0';
-				fileEntry->store(szLine + matchptr[1].rm_so);
+			if (fileEntry->compiled) {
+				if (regexec(fileEntry->compiled, szLine, 2, matchptr, 0) == 0) {
+					szLine[matchptr[1].rm_eo] = '\0';
+					fileEntry->store(szLine + matchptr[1].rm_so);
+				}
 			}
 			fileEntry++;
 		}
 	}
 
-	fileEntry = FILE_ENTRIES;
-	while (fileEntry->regex) {
-		regfree(fileEntry->compiled);
-		fileEntry++;
-	}
+	free_regexes(FILE_ENTRIES);
 
 	fclose(stream);
 }
+
+#undef LINE_BUFFER
