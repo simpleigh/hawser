12624eebfc2378b4bc0655ba34b95ba8d8bb1a67
Leigh Simpson <code@simpleigh.com>
2014-08-15 22:57:58 +0100

    Implemented simple buffer datatype

---
+++ lib/buffer.h
@@ -0,0 +1,22 @@
+#ifndef __BUFFER_H
+#define __BUFFER_H
+
+#include <stddef.h>
+
+typedef struct BUFFER BUFFER;
+
+BUFFER *buffer_create(void);
+
+BUFFER *buffer_from(const char *szString);
+BUFFER *buffer_nfrom(const char *szString, size_t cbString);
+
+size_t buffer_len(const BUFFER *buffer);
+char * buffer_data(const BUFFER *buffer);
+
+size_t buffer_append(BUFFER *buffer, const char *szString);
+size_t buffer_nappend(BUFFER *buffer, const char *szString, size_t cbString);
+
+void buffer_destroy(BUFFER *buffer);
+
+#endif /* __BUFFER_H */
+
