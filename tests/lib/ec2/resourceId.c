5872b01ea21d4e720971938e4cae3c9a6caf191f
Leigh Simpson <code@simpleigh.com>
2014-09-10 23:54:34 +0100

    More testing

--- tests/lib/ec2/resourceId.c
+++ tests/lib/ec2/resourceId.c
@@ -26,6 +26,25 @@ START_TEST(test_read_error_if_pointers_null)
 }
 END_TEST
 
+const char * const INVALID_IDS[] = {
+	"subnet-12345678a", /* too long      */
+	"i12345678",        /* no tag        */
+	"-12345678",        /* tag too short */
+	"subneta-1234567"   /* tag too long  */
+	"i-1234567",        /* ID too short  */
+	"i-123456789",      /* ID too long   */
+	"notag-12345678"    /* unknown tag   */
+};
+
+START_TEST(test_read_errors)
+{
+	HAWSER_EC2_RESOURCE_ID resourceId;
+	const char *string = INVALID_IDS[_i];
+
+	ck_assert_int_eq(HAWSER_INVALID, hawser_ec2_id_read(&resourceId, string));
+}
+END_TEST
+
 int
 main()
 {
@@ -35,6 +54,7 @@ main()
 	int number_failed;
 
 	tcase_add_test(tc_core, test_read_error_if_pointers_null);
+	tcase_add_loop_test(tc_core, test_read_errors, 0, sizeof(INVALID_IDS) / sizeof (char *));
 
 	suite_add_tcase(s, tc_core);
 	sr = srunner_create(s);
